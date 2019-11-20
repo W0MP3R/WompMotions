8402475c1e4f055663671024380437e5bf9c827d
Andrea Cabrero <andreacabrerosolans_189@hotmail.com>
2019-11-20 10:12:56 +0100

    added a re-drawn emotion. Fixed some CSS stuff that wasn't fixed last time
    i don't know why it wasn't fixed :(

--- background.js
+++ background.js
@@ -6,9 +6,9 @@
 
     var emotions = [
       new Emotion('notfunny', "https://i.imgur.com/6RKuegb.jpg"),
-      new Emotion('cry', "TT^TT"),
+      new Emotion('cry', "TT^TT", false),
       new Emotion('scream', "https://i.imgur.com/myazEMI.jpg"),
-      new Emotion('surprise', "https://i.imgur.com/V5Bgq0u.jpg"),
+      new Emotion('surprise', "https://i.imgur.com/gNyGnwB.png"),
       new Emotion('blankface', "https://i.imgur.com/DhCCvgc.png"),
       new Emotion('sad', "https://i.imgur.com/5R9wPkp.jpg"),
       new Emotion('todaypaella', "https://i.imgur.com/VPzhSHo.jpg"),
@@ -20,7 +20,8 @@
       new Emotion('frustration', "https://i.imgur.com/BJBHlDr.jpg"),      
       new Emotion('bored', "https://i.imgur.com/5DNrFK7.png"),  
       new Emotion('shruggie', "https://i.stack.imgur.com/8MXJQ.jpg"),
-      new Emotion('hiya', "https://i.imgur.com/Llzc0ED.jpg")
+      new Emotion('hiya', "https://i.imgur.com/Llzc0ED.jpg"),
+      new Emotion('surprise_old', "https://i.imgur.com/V5Bgq0u.jpg")
   ];
 
 
--- emotions.js
+++ emotions.js
@@ -8,7 +8,7 @@ var emotions = [
     new Emotion('notfunny', "https://i.imgur.com/6RKuegb.jpg"),
     new Emotion('cry', "TT^TT", false),
     new Emotion('scream', "https://i.imgur.com/myazEMI.jpg"),
-    new Emotion('surprise', "https://i.imgur.com/V5Bgq0u.jpg"),
+    new Emotion('surprise', "https://i.imgur.com/gNyGnwB.png"),
     new Emotion('blankface', "https://i.imgur.com/DhCCvgc.png"),
     new Emotion('sad', "https://i.imgur.com/5R9wPkp.jpg"),
     new Emotion('todaypaella', "https://i.imgur.com/VPzhSHo.jpg"),
@@ -20,7 +20,8 @@ var emotions = [
     new Emotion('frustration', "https://i.imgur.com/BJBHlDr.jpg"),      
     new Emotion('bored', "https://i.imgur.com/5DNrFK7.png"),  
     new Emotion('shruggie', "https://i.stack.imgur.com/8MXJQ.jpg"),
-    new Emotion('hiya', "https://i.imgur.com/Llzc0ED.jpg")
+    new Emotion('hiya', "https://i.imgur.com/Llzc0ED.jpg"),
+    new Emotion('surprise_old', "https://i.imgur.com/V5Bgq0u.jpg")
 ];
 
 function paintEmotions() {
@@ -40,10 +41,12 @@ function paintEmotions() {
 
         emotionItemList.appendChild(emotionName);
 
+        var emotionPic = document.createElement('div');
+        emotionPic.className = 'emotion-pic-wrapper';
+
         if(emotions[i].isPic == true)
         {
-            var emotionPic = document.createElement('div');
-            emotionPic.className = 'emotion-pic-wrapper';
+            
 
             var emotionPicIMG = document.createElement('img');
             emotionPicIMG.className = 'pics';
@@ -54,11 +57,8 @@ function paintEmotions() {
         }
         else
         {
-            var emotionPic = document.createElement('div');
-            emotionPic.className = 'emotion-name';
-
             var emotionPicSpan = document.createElement('span');
-            emotionPicSpan.innerHTML = '##' + emotions[i].description;
+            emotionPicSpan.innerHTML = emotions[i].description;
             emotionPic.appendChild(emotionPicSpan);
 
             emotionItemList.appendChild(emotionPic);
